632e0e4826cdb1c6e6aacbe6d76932205ec04272
wjwitek <weronikaj.witek@gmail.com>
2022-05-18 14:16:35 +0200

    very basic gui, loads map

---
+++ src/main/java/gui/Board.java
@@ -0,0 +1,120 @@
+package gui;
+
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Serial;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class Board extends JComponent{
+    @Serial
+    private static final long serialVersionUID = 1L;
+    Point[][] points;
+    private final int size;
+    final String mapSource = "src/main/resources/table.txt";
+
+    public Board(int length, int height, int squareSize) {
+        size = squareSize;
+        setBackground(Color.WHITE);
+        setOpaque(true);
+        try {
+            initialize((length / size) + 1, (height / size) + 1);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void clear() {
+        for (Point[] pp : points)
+            for (Point p : pp) {
+                p.clear();
+            }
+        this.repaint();
+    }
+
+    private void initialize(int length, int height) throws IOException {
+        points = new Point[length][height];
+        for (int x = 0; x < points.length; ++x)
+            for (int y = 0; y < points[x].length; ++y)
+                points[x][y] = new Point();
+
+        FileReader filereader = new FileReader(mapSource);
+        BufferedReader bufferedreader = new BufferedReader(filereader);
+        String line = bufferedreader.readLine();
+        while (line != null) {
+            int x = 0, y = 0, idx=0;
+            while(idx<line.length()){
+                if(line.charAt(idx)==']') {
+                    y += 1;
+                    x=-1;
+                }
+                if(line.charAt(idx)==',')
+                    x+=1;
+                if(line.charAt(idx)>='0' && line.charAt(idx)<='9' &&(y<points.length && x<points[0].length)) {
+                    points[y][x].type = line.charAt(idx)-48;
+                }
+                idx++;
+            }
+            line = bufferedreader.readLine();
+        }
+    }
+
+    protected void paintComponent(Graphics g) {
+        if (isOpaque()) {
+            g.setColor(getBackground());
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        }
+        g.setColor(Color.GRAY);
+        drawNetting(g, size);
+    }
+
+    private void drawNetting(Graphics g, int gridSpace) {
+        Insets insets = getInsets();
+        int firstX = insets.left;
+        int firstY = insets.top;
+        int lastX = this.getWidth() - insets.right;
+        int lastY = this.getHeight() - insets.bottom;
+
+        int x = firstX;
+        while (x < lastX) {
+            g.drawLine(x, firstY, x, lastY);
+            x += gridSpace;
+        }
+
+        int y = firstY;
+        while (y < lastY) {
+            g.drawLine(firstX, y, lastX, y);
+            y += gridSpace;
+        }
+
+        for (x = 1; x < points.length-1; ++x) {
+            for (y = 1; y < points[x].length-1; ++y) {
+                switch (points[x][y].type) {
+                    case 0 -> g.setColor(new Color(1.0f, 1.0f, 1.0f, 1.0f));
+                    case 1 -> g.setColor(new Color(1.0f, 0.0f, 0.0f, 0.7f));
+                    case 2 -> g.setColor(new Color(0.0f, 1.0f, 0.0f, 0.7f));
+                    case 3 -> g.setColor(new Color(0.2f, 0.2f, 1.0f, 0.7f));
+                    case 4 -> g.setColor(new Color(0.2f, 0.2f, 0.2f, 0.7f));
+                    case 5 -> g.setColor(new Color(0.5f, 0.5f, 0.5f, 0.7f));
+                    case 6 -> g.setColor(new Color(0.0f, 0.5f, 0.8f, 0.7f));
+                    case 7 -> g.setColor(new Color(0.6f, 0.2f, 0.4f, 0.7f));
+                    case 8 -> g.setColor(new Color(0.8f, 0.2f, 0.0f, 0.7f));
+                    default -> g.setColor(new Color(0.0f, 0.0f, 0.0f, 0.7f));
+                }
+                g.fillRect((x * size) + 1, (y * size) + 1, (size - 1), (size - 1));
+            }
+        }
+
+    }
+    public void iteration(){
+        // TODO
+        System.out.println("Something happens in simulation.");
+    }
+
+}
---
+++ src/main/java/gui/GUI.java
@@ -0,0 +1,96 @@
+package gui;
+
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.Serial;
+
+public class GUI extends JPanel implements ActionListener, ChangeListener {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Board board;
+    private JButton startStopSimulation;
+    private JSlider simulationSpeed;
+    int squareSize;
+    private final int maxDelay = 20; //TODO set higher limit after creating simulation, for now, with nothing to do, higher values crash app
+    private final Timer timer;
+    private int iterNum = 0;
+    private final JFrame frame;
+
+    public GUI(JFrame jf, int squareSize) {
+        jf.setResizable(false);
+        frame = jf;
+        timer = new Timer(maxDelay / 5, this);
+        timer.stop();
+        this.squareSize = squareSize;
+    }
+
+    public void initialize(Container container) {
+        container.setLayout(new BorderLayout());
+        container.setSize(new Dimension(1024, 768));
+
+        JPanel buttonPanel = new JPanel();
+
+        startStopSimulation = new JButton("Start");
+        startStopSimulation.setActionCommand("Start");
+        startStopSimulation.addActionListener(this);
+
+        simulationSpeed = new JSlider();
+        simulationSpeed.setMinimum(0);
+        simulationSpeed.setMaximum(maxDelay);
+        simulationSpeed.addChangeListener(this);
+        simulationSpeed.setValue(maxDelay - timer.getDelay());
+
+        JButton restart = new JButton("Restart");
+        restart.setActionCommand("Restart");
+        restart.addActionListener(this);
+
+        buttonPanel.add(startStopSimulation);
+        buttonPanel.add(simulationSpeed);
+        buttonPanel.add(restart);
+
+        board = new Board(1024, 768 - buttonPanel.getHeight(), squareSize);
+        container.add(board, BorderLayout.CENTER);
+        container.add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(timer)){
+            iterNum++;
+            frame.setTitle("Traffic simulation, iteration: " + iterNum);
+            board.iteration();
+        }
+        else {
+            String command = e.getActionCommand();
+            switch (command) {
+                case "Start" -> {
+                    timer.start();
+                    startStopSimulation.setText("Stop");
+                    startStopSimulation.setActionCommand("Stop");
+                }
+                case "Stop" -> {
+                    timer.stop();
+                    startStopSimulation.setText("Start");
+                    startStopSimulation.setActionCommand("Start");
+                }
+                case "Restart" -> {
+                    timer.stop();
+                    startStopSimulation.setText("Start");
+                    startStopSimulation.setActionCommand("Start");
+                    System.out.println("Restarting simulation");
+                    // TODO actually restart simulation
+                    iterNum = 0;
+                    frame.setTitle("Traffic simulation, iteration: " + iterNum);
+                }
+            }
+        }
+    }
+
+    public void stateChanged(ChangeEvent a){
+        timer.setDelay(maxDelay - simulationSpeed.getValue());
+    }
+}
