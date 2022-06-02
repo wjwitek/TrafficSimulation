f366b9581658cc520ee483b789aec59f6a31d4e8
wjwitek <weronikaj.witek@gmail.com>
2022-06-02 17:33:47 +0200

    first try of queue on spawn point

--- src/main/java/model/spawn/CarSpawnPoint.java
+++ src/main/java/model/spawn/CarSpawnPoint.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 
 public class CarSpawnPoint extends SpawnPoint {
     private final ArrayList<Integer> possibleDestinationProbability = new ArrayList<>();
+    private ArrayList<Car> queue = new ArrayList<>();
     public CarSpawnPoint(JSONObject info, Board newMap){
         super(new Coords(0, 0), 0, null);
         maxVelocity = 4; //TODO changeable
@@ -21,14 +22,14 @@ public class CarSpawnPoint extends SpawnPoint {
         for (int i=0; i<destinationCoords.length(); i += 2){
             possibleDestination.add(new Coords(destinationCoords.getInt(i), destinationCoords.getInt(i + 1)));
             possibleDestinationProbability.add(destinationCoordsProbability.getInt(i/2));
-            System.out.println(destinationCoords.getInt(i) + " " + destinationCoords.getInt(i + 1) + "; "
-                    + destinationCoordsProbability.getInt(i/2));
+//            System.out.println(destinationCoords.getInt(i) + " " + destinationCoords.getInt(i + 1) + "; "
+//                    + destinationCoordsProbability.getInt(i/2));
         }
         map = newMap;
     }
 
     public Car get(){
-      if (getRandomNumber(0, 101) < spawn_probability && !(map.getPointByCoords(coords).hasCar)){
+      if (getRandomNumber(0, 101) < spawn_probability){
           Car newCar = new Car(map, getRandomNumber(1, maxVelocity + 1), coords.copy(), maxVelocity);
           // TODO change to random generation
           int destination_no = getRandomNumber(0, 101);
@@ -41,8 +42,17 @@ public class CarSpawnPoint extends SpawnPoint {
               }
           }
           newCar.destination = possibleDestination.get(idx);
+          if (map.getPointByCoords(coords).hasCar){
+              queue.add(newCar);
+              return null;
+          }
           return newCar;
        }
-        return null;
+      else if (queue.size() != 0){
+          Car newCar = queue.get(0);
+          queue.remove(0);
+          return newCar;
+      }
+      return null;
     }
 }
